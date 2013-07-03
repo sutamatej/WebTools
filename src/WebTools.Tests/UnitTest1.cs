e450dbfde4ddf99b928bc49f9559da2f678fa59a
Matej Suta <suta.matej@gmail.com>
2013-07-03 04:41:12 -0700

    initial project structure

---
+++ src/WebTools.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace WebTools.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
