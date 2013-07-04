d65b6c740452d4bfbbba76317b91f58f882d5bd8
Matej Suta <suta.matej@gmail.com>
2013-07-04 09:32:01 +0200

    added dropdown list helper

---
+++ src/WebTools.Tests/Helpers/DropDownListHelperTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebTools.Helpers;
+using Xunit;
+
+namespace WebTools.Tests.Helpers
+{
+    public class DropDownListHelperTests
+    {
+        [Fact]
+        public void Dropdown_list_helper_renders_html()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList).ToString();
+            Assert.Equal("<select id=\"Value\" name=\"Value\"><option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+
+        [Fact]
+        public void Dropdown_list_helper_renders_default_option()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList, "default").ToString();
+            Assert.Equal("<select id=\"Value\" name=\"Value\"><option value=\"\">default</option>\r\n<option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+
+        [Fact]
+        public void Dropdown_list_helper_renders_class_attribute()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList).Class("some-class").ToString();
+            Assert.Equal("<select class=\"some-class\" id=\"Value\" name=\"Value\"><option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+
+        [Fact]
+        public void Dropdown_list_helper_renders_id_attribute()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList).Id("some-id").ToString();
+            Assert.Equal("<select id=\"some-id\" name=\"Value\"><option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+
+        [Fact]
+        public void Dropdown_list_helper_renders_disabled_attribute()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList).Disabled(true).ToString();
+            Assert.Equal("<select disabled=\"disabled\" id=\"Value\" name=\"Value\"><option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+
+        [Fact]
+        public void Dropdown_list_helper_supports_attribute_combinations()
+        {
+            var model = new DropDownListTestModel();
+            var helper = HtmlHelperBuilder.GetHtmlHelper(model);
+            var result = helper.DropDownList(m => m.Value, model.SelectList, "default").Id("test-id").Class("test-class").Disabled(true).ToString();
+            Assert.Equal("<select class=\"test-class\" disabled=\"disabled\" id=\"test-id\" name=\"Value\"><option value=\"\">default</option>\r\n<option value=\"1\">a</option>\r\n<option value=\"2\">b</option>\r\n<option value=\"3\">c</option>\r\n</select>", result);
+        }
+    }
+
+    public class DropDownListTestModel
+    {
+        public int Value { get; set; }
+
+        public IEnumerable<SelectListItem> SelectList
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "a", Value = "1" },
+                    new SelectListItem { Text = "b", Value = "2", Selected = true },
+                    new SelectListItem { Text = "c", Value = "3" }
+                };
+            }
+        }
+    }
+}
---
+++ src/WebTools/Helpers/DropDownListHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace WebTools.Helpers
+{
+    public static class DropDownListHelper
+    {
+        public static IDropDownList DropDownList<TModel, TProperty>(
+            this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> property,
+            IEnumerable<SelectListItem> items,
+            string defaultItem = null)
+        {
+            return new DropDownList<TModel, TProperty>(helper, property, items, defaultItem);
+        }
+    }
+
+    public class DropDownList<TModel, TProperty> : InputBase<TModel, IDropDownList>, IDropDownList
+    {
+        private Expression<Func<TModel, TProperty>> _property;
+        private IEnumerable<SelectListItem> _items;
+        private string _defaultItem;
+
+        public DropDownList(
+            HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> property,
+            IEnumerable<SelectListItem> items,
+            string defaultItem)
+            : base(helper)
+        {
+            _property = property;
+            _items = items;
+            _defaultItem = defaultItem;
+            _elementInstance = this;
+        }
+
+        public override string ToString()
+        {
+            var htmlString = _helper.DropDownListFor(_property, _items, _defaultItem, _htmlAttributes);
+            return htmlString.ToString();
+        }
+    }
+
+    public interface IDropDownList : IInput<IDropDownList>
+    {
+
+    }
+}
