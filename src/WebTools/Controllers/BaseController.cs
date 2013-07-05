617c2e03c13ee2421a8b1ea690149eb5ea781c4c
Matej Suta <suta.matej@gmail.com>
2013-07-05 07:58:10 +0200

    added base controller

---
+++ src/WebTools/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using WebTools.Routing;
+
+namespace WebTools.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected RedirectToRouteResult RedirectToAction<TController>(
+            Expression<Action<TController>> action)
+            where TController : Controller
+        {
+            var webRoute = new WebRoute<TController>(action);
+            return RedirectToAction(webRoute.Action, webRoute.Controller, webRoute.Values);
+        }
+    }
+}
