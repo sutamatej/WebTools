2923738a2f1f41cdcfd180816ac0199fd94541b4
Matej Suta <suta.matej@gmail.com>
2013-07-06 09:48:25 +0200

    added radio button helper

---
+++ src/WebTools.Tests/Helpers/RadioButtonHelperTests.cs
@@ -0,0 +1,68 @@
+﻿using WebTools.Helpers;
+using Xunit;
+
+namespace WebTools.Tests.Helpers
+{
+    public class RadioButtonHelperTests
+    {
+        [Fact]
+        public void Radio_button_helper_renders_html()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.Second).ToHtmlString();
+            Assert.Equal("<input id=\"Value\" name=\"Value\" type=\"radio\" value=\"Second\" />", result);
+        }
+
+        [Fact]
+        public void Radio_button_helper_renders_class_attribute()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.First).Class("test-class").ToHtmlString();
+            Assert.Equal("<input checked=\"checked\" class=\"test-class\" id=\"Value\" name=\"Value\" type=\"radio\" value=\"First\" />", result);
+        }
+
+        [Fact]
+        public void Radio_button_helper_renders_id_attribute()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.Third).Id("test-id").ToHtmlString();
+            Assert.Equal("<input id=\"test-id\" name=\"Value\" type=\"radio\" value=\"Third\" />", result);
+        }
+
+        [Fact]
+        public void Radio_button_helper_renders_disabled_attribute()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.Second).Disabled(true).ToHtmlString();
+            Assert.Equal("<input disabled=\"disabled\" id=\"Value\" name=\"Value\" type=\"radio\" value=\"Second\" />", result);
+        }
+
+        [Fact]
+        public void Radio_button_helper_renders_checked_attribute()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.Third).Checked(true).ToHtmlString();
+            Assert.Equal("<input checked=\"checked\" id=\"Value\" name=\"Value\" type=\"radio\" value=\"Third\" />", result);
+        }
+
+        [Fact]
+        public void Radio_button_helper_supports_attribute_combinations()
+        {
+            var helper = HtmlHelperBuilder.GetHtmlHelper(new RadioButtonTestModel());
+            var result = helper.RadioButton(m => m.Value, RadioButtonTestEnum.First).Id("some-id").Disabled(true).Class("some-class").Checked(true).ToHtmlString();
+            Assert.Equal("<input checked=\"checked\" class=\"some-class\" disabled=\"disabled\" id=\"some-id\" name=\"Value\" type=\"radio\" value=\"First\" />", result);
+        }
+    }
+
+    public class RadioButtonTestModel
+    {
+        public RadioButtonTestEnum Value { get; set; }
+    }
+
+    public enum RadioButtonTestEnum
+    {
+        First,
+        Second,
+        Third
+    }
+}
---
+++ src/WebTools/Helpers/RadioButtonHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace WebTools.Helpers
+{
+    public static class RadioButtonHelper
+    {
+        public static ICheckable RadioButton<TModel, TProperty>(
+            this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> property,
+            TProperty value)
+        {
+            return new RadioButton<TModel, TProperty>(helper, property, value);
+        }
+    }
+
+    public class RadioButton<TModel, TProperty> : Input<TModel, ICheckable>, ICheckable
+    {
+        private Expression<Func<TModel, TProperty>> _property;
+        private TProperty _value;
+
+        public RadioButton(
+            HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> property,
+            TProperty value)
+            : base(helper)
+        {
+            _property = property;
+            _value = value;
+            _elementInstance = this;
+        }
+
+        public ICheckable Checked(bool @checked)
+        {
+            if (@checked)
+                _htmlAttributes.Add(Constants.Checked, Constants.Checked);
+            return this;
+        }
+
+        public string ToHtmlString()
+        {
+            var htmlString = _helper.RadioButtonFor(_property, _value, _htmlAttributes);
+            return htmlString.ToString();
+        }
+    }
+}
